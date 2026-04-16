6c2d07cb7ac8f93d9ed76f11fcefd3bdea759cf5
xing75967 <unsv1985@gmail.com>
2026-04-15 18:23:21 -0600

    Create credentials.js

---
+++ js/credentials.js
@@ -0,0 +1,16 @@
+window.DB = {
+  users: {
+    admin: {
+      password: "admin",
+      grades: ["all"]
+    },
+    sivan: {
+      password: "sivan123",
+      grades: ["grade3"]
+    },
+    alice: {
+      password: "1234",
+      grades: ["grade4"]
+    }
+  }
+};
