965b6b6266d0982590625553a1ba36e4217e4ee3
xing75967 <unsv1985@gmail.com>
2026-04-15 18:31:33 -0600

    Create db.js

---
+++ js/db.js
@@ -0,0 +1,7 @@
+window.DB = {
+  users: {
+    admin: { password: "admin", grades: ["all"] },
+    sivan: { password: "sivan123", grades: ["grade3"] },
+    user1: { password: "1234", grades: ["grade4"] }
+  }
+};
